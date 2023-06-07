d301ac4b4a9c2274f8346477e69f1dec5fe3b8f4
Sashko P <sashko_peshevski@discovery.com>
2023-06-07 13:09:40 +0200

    Refresh button

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useReducer, useState} from 'react'
 import logo from './img/p4.png';
 import './App.css';
 import {pico4Apps} from './pico4Apps'
 
 function App() {
     const [apps, setApps] = useState<any[]>([]);
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
 
     useEffect(() => {
         pico4Apps.then(apps => setApps(apps));
-    }, []);
+    }, [ignored]);
+
+    function handleClick() {
+        forceUpdate();
+    }
 
     return (
         <div className="App">
             <header className="App-header">
+                <button className="button" onClick={handleClick}>Refresh</button>
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>List of Pico 4 Apps</p>
             </header>
@@ -22,6 +28,7 @@ function App() {
                     return <div className="Div-flex">
                         <div><img src={app.logo}/></div>
                         {app.name}
+                        <br/><br/>
                     </div>;
                 })
             }
